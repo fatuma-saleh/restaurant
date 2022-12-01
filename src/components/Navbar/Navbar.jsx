bc9d30b22cfe74746efd200abf5e6c311a77ac05
Fatuma <sfatex@gmail.com>
2022-12-01 13:30:00 -0500

    Add important links that appear at the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,20 @@ import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import images from '../../constants/images';
 const Navbar = () => (
   <nav className='app_navbar'>
-    Navbar
+    <div className='app_navbar-logoi'>
+      <img src={images.gericht} alt="app logo"/>
+    </div>
+    <ul className='app_navbar-links'>
+      <li className='p_opensans'><a href="#home">Home</a></li>
+
+      
+      <li className='p_opensans'><a href="#about">About</a></li>
+      <li className='p_opensans'><a href="#menu">Menu</a></li>
+      <li className='p_opensans'><a href="#awards">Awards</a></li>
+      <li className='p_opensans'><a href="#contact">Contact</a></li>
+     
+
+    </ul>
   </nav>
 );
 
