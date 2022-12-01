23bc5ca7d8395f82f6a00f763b6b188afa6dd771
Fatuma <sfatex@gmail.com>
2022-12-01 13:44:27 -0500

    change background and use flex for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import './Navbar.css';
-import{ GiHamburgerMenu } from 'react-icons/gi';
-import { MdOutlineRestaurantMenu } from 'react-icons/md';
-import images from '../../constants/images';
+import "./Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import images from "../../constants/images";
 const Navbar = () => (
-  <nav className='app_navbar'>
-    <div className='app_navbar-logoi'>
-      <img src={images.gericht} alt="app logo"/>
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="app logo" />
     </div>
-    <ul className='app_navbar-links'>
-      <li className='p_opensans'><a href="#home">Home</a></li>
-
+    <ul className="app__navbar-links">
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>
       
-      <li className='p_opensans'><a href="#about">About</a></li>
-      <li className='p_opensans'><a href="#menu">Menu</a></li>
-      <li className='p_opensans'><a href="#awards">Awards</a></li>
-      <li className='p_opensans'><a href="#contact">Contact</a></li>
-     
-
+      <li className="p__opensans">
+        <a href="#about">About</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#menu">Menu</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#awards">Awards</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#contact">Contact</a>
+      </li>
     </ul>
   </nav>
 );
